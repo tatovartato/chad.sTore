cd03610db0bbfb391b4b126b93f5cbd1bec6480e
nika <fantozy.v2@gmail.com>
2025-02-03 22:13:24 +0400

    Added: fetching reviews for each product

--- products/serializers.py
+++ products/serializers.py
@@ -2,22 +2,15 @@ from rest_framework import serializers
 from products.models import Review, Product
 
 
-class ProductSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    description = serializers.CharField()
-    price = serializers.FloatField()
-    currency = serializers.ChoiceField(choices=['GEL', 'USD', 'EUR'])
-
-
-class ReviewSerializer(serializers.Serializer):
+class ReviewSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField(write_only=True)
-    content = serializers.CharField()
-    rating = serializers.IntegerField()
+
+    class Meta:
+        model = Review
+        fields = ['product_id', 'content', 'rating']
 
     def validate_product_id(self, value):
-        try:
-            Product.objects.get(id=value)
-        except Product.DoesNotExist:
+        if not Product.objects.filter(id=value).exists():
             raise serializers.ValidationError("Invalid product_id. Product does not exist.")
         return value
 
@@ -27,14 +20,16 @@ class ReviewSerializer(serializers.Serializer):
         return value
 
     def create(self, validated_data):
-        product = Product.objects.get(id=validated_data['product_id'])
+        product = Product.objects.get(id=validated_data.pop('product_id'))
         user = self.context['request'].user
+        return Review.objects.create(product=product, user=user, **validated_data)
 
-        review = Review.objects.create(
-            product=product,
-            user=user,
-            content=validated_data['content'],
-            rating=validated_data['rating'],
-        )
-        return review
+
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        exclude = ['created_at', 'updated_at', 'tags'] 
+        model = Product
+
+
     
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from products.views import product_view, review_view
+from products.views import products_view, reviews_view, product_view
 
 urlpatterns = [
-    path('products/', product_view, name="products"),
-    path('reviews/', review_view, name="reviews")
+    path('products/', products_view, name="products"),
+    path('products/<int:pk>', product_view, name='product'),
+    path('reviews/', reviews_view, name="reviews")
 ]
