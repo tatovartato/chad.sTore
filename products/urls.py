2ae21364187ddda60f8540be1b08548be58a8239
nika <fantozy.v2@gmail.com>
2025-02-13 01:11:23 +0400

    Added: views in urls.py (TagList, FavoriteProductViewSet, CartViewSet)

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from products.views import ProductViewSet, ReviewViewSet, FavoriteProductViewSet, CartViewSet
+from products.views import ProductViewSet, ReviewViewSet, FavoriteProductViewSet, CartViewSet, TagList
 
 urlpatterns = [
     path('products/', ProductViewSet.as_view(), name="products"),
     path('products/<int:pk>', ProductViewSet.as_view(), name='product'),
     path('reviews/', ReviewViewSet.as_view(), name="reviews"),
     path('favorite_products/', FavoriteProductViewSet.as_view(), name='favorite_products'),
-    path('cart/', CartViewSet.as_view(), name='cart')
+    path('favorite_products/<int:pk>/', FavoriteProductViewSet.as_view(), name='favorite_product'),
+    path('cart/', CartViewSet.as_view(), name='cart'),
+    path('tags/', TagList.as_view(), name='tags')
 ]
