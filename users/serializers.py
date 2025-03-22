8404781262204ed35b8fdcccbbe7152ba5fc4fcf
Tato Mosidze <mosidzetato@gmail.com>
2025-03-22 19:39:07 +0400

    Added ProfileViewSet with authentication and permissions

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from users.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -20,10 +20,33 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password":"password dont match"})
+            raise serializers.ValidationError({"password": "Passwords do not match"})
         return attrs
-        
+
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        validated_data['password'] = make_password(validated_data['password'])
+        user = User.objects.create(**validated_data)
         return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    email = serializers.EmailField(required=False)
+    phone_number = serializers.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'phone_number']
+
+    def validate_password(self, value):
+        if value:
+            return make_password(value)
+        return value
+
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.password = make_password(password)
+        return super().update(instance, validated_data)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, ProfileSerializer
+from products.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 User = get_user_model()
 
@@ -10,8 +14,29 @@ class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = RegisterSerializer
 
 class UserViewSet(mixins.ListModelMixin, 
-                            mixins.RetrieveModelMixin, 
-                            viewsets.GenericViewSet):
+                  mixins.RetrieveModelMixin, 
+                  viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+class ProfilViewSet(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.PartialUpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_object(self):
+        return self.request.user
+    
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+     
+    def perform_destroy(self, instance):
+        instance.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
